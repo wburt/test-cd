a46e50a6901ff0a6773e8f6593fca80b3ee6bd9c
Will Burt <william.burt@gov.bc.ca>
2023-04-24 11:34:49 -0700

    logging

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import os
 from datetime import datetime
+import logging
 
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
+logger.info("Starting")
 now = datetime.now()
 data = 'data/app-data.txt'
 assert os.path.exists(data)
 with open(data,'w') as f:
     f.writelines([f"{now.strftime('%m/%d/%Y %H:%M:%S')}\t Run with {os.cpu_count()} cpu"])
+logger.info("Completed")
